40aa2bdda8d21a6885c80058c88dd34d99c3f112
asifkhan261 <asifkhan261@gmail.com>
2021-11-21 14:13:36 +0530

    aave done dynamic ui

--- Assets/UI/CentreMap/Buildings/Aave/AaveUI.cs
+++ Assets/UI/CentreMap/Buildings/Aave/AaveUI.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class AaveUI : MonoBehaviour
 {       
     public GameObject AaveUI2;
+    public GameObject AaveUIText;
     /// <summary>
     /// OnTriggerEnter is called when the Collider other enters the trigger.
     /// </summary>
     /// <param name="other">The other Collider involved in this collision.</param>
     void OnTriggerEnter(Collider other)
     {
-        AaveUI2.SetActive(true); 
+        AaveUIText.SetActive(true);
     }
     void OnTriggerExit(Collider other) {
-        AaveUI2.SetActive(false); 
+        AaveUIText.SetActive(false); 
+    }
+    void Update()
+    {
+        if(AaveUIText.activeSelf ==true){
+        if(Input.GetKeyUp(KeyCode.F) ){
+           AaveUIText.SetActive(false); 
+           ToggleAaveUI();
+        }
+        }
+    }
+    private void ToggleAaveUI(){
+        if(AaveUI2.activeSelf == true){
+            AaveUI2.SetActive(false);
+            AaveUIText.SetActive(true);   
+        }else{
+            AaveUI2.SetActive(true);
+        }
     }
     // Start is called before the first frame update
 }
